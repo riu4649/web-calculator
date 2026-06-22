b16a5bea1f7f7bc26091dc54cfad9ee1333ab8d0
riu <rai10riu@gmail.com>
2026-06-22 16:38:48 +0900

    test

--- webapp/static/script.js
+++ webapp/static/script.js
@@ -1,4 +1,5 @@
 let aftercalculate = false;
+let count = 0;
 console.log(aftercalculate);
 
 function addNumber(number) {
@@ -14,7 +15,7 @@ function addNumber(number) {
 
 function addOperator(operator) {
             let display = document.getElementById("display");
-            display.value = display.value + operator;
+            display.value = display.value + " " + operator + " ";
 }
 
 function clearDisplay() {
@@ -38,17 +39,46 @@ function backspace() {
             display.value = display.value.slice(0, display.value.length - 1);
 }
 
+function fontsize() {
+    let display = document.getElementById("display");
+
+    if (display.value.length <= 15) {
+        display.style.fontSize = "35px";
+    } else if (display.value.length > 25) {
+        display.style.fontSize = "20px";
+    } else if (display.value.length > 20) {
+        display.style.fontSize = "24px";
+    } else if (display.value.length > 15) {
+        display.style.fontSize = "29px";
+    }
+}; 
+
+function fontsize2() {
+    let display = document.getElementById("display");
+
+    if (display.value.length <= 15) {
+        display.style.fontSize = "35px";
+    } else if (display.value.length > (15 + count * 5)) {
+        display.style.fontSize = (display.style.fontSize - (6 - count)) + "px";
+        count += 1;
+    }
+}
+display.value.length > (15 + count * 5)
+
 document.addEventListener("keydown", function(event) {
     console.log(event.key);
     if ("0123456789".includes(event.key)) {
         addNumber(event.key);
+        fontsize();
     }
     
     if ("+-*/".includes(event.key)) {
         addOperator(event.key);
+        fontsize()
     }
     if (event.key === "Backspace") {
         backspace();
+        fontsize();
     }
     if (event.key === "Enter") {
         calculate();
